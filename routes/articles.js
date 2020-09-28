3fa073ad5d7e9f391f9c446d50f806b27f27ab1c
whitefang-asus <sankitpramod.min18@itbhu.ac.in>
2020-09-28 17:09:04 +0530

    added delete route and slug

--- routes/articles.js
+++ routes/articles.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { findByIdAndDelete } = require("./../models/article");
 const router = express.Router();
 const Article = require("./../models/article");
 
@@ -6,8 +7,9 @@ router.get("/new", (req, res) => {
   res.render("articles/new");
 });
 
-router.get("/:id", async (req, res) => {
-  const article = await Article.findById(req.params.id);
+router.get("/:slug", async (req, res) => {
+  const article = await Article.findOne({ slug: req.params.slug });
+
   if (article === null) res.redirect("/");
   res.render("articles/show", { article: article });
 });
@@ -21,10 +23,15 @@ router.post("/", async (req, res) => {
 
   try {
     artic = await article.save();
-    res.redirect(`/articles/${artic.id}`);
+    res.redirect(`/articles/${artic.slug}`);
   } catch (e) {
-    res.render("/articles/new", { arti: arti });
+    res.render("/articles/new", { artic: artic });
   }
+});
+
+router.delete("/:id", async (req, res) => {
+  await Article.findByIdAndDelete(req.params.id);
+  res.redirect("/");
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,16 +3,19 @@ const app = express();
 const articleRoute = require("./routes/articles");
 const mongoose = require("mongoose");
 const Article = require("./models/article");
+const methodOverride = require("method-override");
 
 mongoose
   .connect("mongodb://127.0.0.1/Markdown", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then(console.log("connected to database"));
 
 const port = 3000;
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.get("/", async (req, res) => {
   const articles = await Article.find().sort({
