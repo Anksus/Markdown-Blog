04fe086b878db73035ce9bc7dc304dc2fc1258df
whitefang-asus <sankitpramod.min18@itbhu.ac.in>
2020-09-28 13:38:15 +0530

    new route added

--- routes/articles.js
+++ routes/articles.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const Article = require("./../models/article");
 
 router.get("/new", (req, res) => {
   res.render("articles/new");
 });
 
-router.post("/", (req, res) => {
-  res.send("hello world");
+router.get("/:id", async (req, res) => {
+  const article = await Article.findById(req.params.id);
+  if (article === null) res.redirect("/");
+  res.render("articles/show", { article: article });
+});
+
+router.post("/", async (req, res) => {
+  let article = new Article({
+    title: req.body.title,
+    description: req.body.description,
+    markdown: req.body.markdown,
+  });
+
+  try {
+    artic = await article.save();
+    res.redirect(`/articles/${artic.id}`);
+  } catch (e) {
+    res.render("/articles/new", { arti: arti });
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
 const articleRoute = require("./routes/articles");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1/Markdown", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("connected to database"));
 
 const port = 3000;
-
+app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
-app.use("/articles/", articleRoute);
 app.get("/", (req, res) => {
   const articles = [
     {
@@ -22,5 +29,7 @@ app.get("/", (req, res) => {
 
   res.render("articles/index", { articles: articles });
 });
+
+app.use("/articles/", articleRoute);
 
 app.listen(port, () => console.log(`Example app listening on ${port} port!`));
